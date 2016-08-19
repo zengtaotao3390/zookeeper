6601c8cacd040e28f54c4f67f27d517622b822e5
zengtaotao3390 <378041351@qq.com>
2016-08-19 10:04:38 +0800

    测试watch回调时，由于自身连接关闭而触发的session close 警告异常

--- src/main/java/com/peach/zk/apacheLock/Driver.java
+++ src/main/java/com/peach/zk/apacheLock/Driver.java
@@ -24,7 +24,6 @@ public class Driver {
                 @Override
                 public void lockAcquired() throws InterruptedException {
                     logger.info("太开心了，我获得锁了");
-                    Thread.sleep(10000);
                     countDownLatch.countDown();
                 }
 
@@ -34,7 +33,10 @@ public class Driver {
                 }
             });
             writeLock.lock();
-            countDownLatch.await(120000, TimeUnit.MILLISECONDS);
+            //如果等待时间过少，当前连接已经删除了，而后面watch首节点变化，那么回调之后，会出现一个警告一次，告诉你连接已经关闭。
+            //虽然这样做不影响正常的业务逻辑，但是也不好看，有不合理的地方，后面准备修改为不用session为key的Znode，而是使用固定的
+            //然后使用一个连接不停的请求，不用每次都去建立一个连接，可以避免这样的问题发生。
+            countDownLatch.await(6000, TimeUnit.MILLISECONDS);
             zk.close();
         }
 
